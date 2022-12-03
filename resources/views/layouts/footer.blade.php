766be1409314baa2ef36b5efb27cc21fef8bf372
Lilkizarmie <jefferyadolor18@gmail.com>
2022-12-03 13:58:52 +0100

    page setup completed

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,50 @@
+ <!-- footer -->
+ <footer>
+  <div class="container">
+    <div class="sb-footer-frame">
+      <a href="home.html" class="sb-logo-frame">
+        <!-- logo img -->
+        <img src="img/ui/logo.png" alt="Starbelly">
+      </a>
+      <ul class="sb-social">
+        <li><a href="#."><i class="fab fa-twitter"></i></a></li>
+        <li><a href="#."><i class="fab fa-instagram"></i></a></li>
+        <li><a href="#."><i class="fab fa-facebook-f"></i></a></li>
+        <li><a href="#."><i class="fab fa-youtube"></i></a></li>
+      </ul>
+      <div class="sb-copy">&copy; 2022 Cheffemi. All Rights Reserved.</div>
+    </div>
+  </div>
+</footer>
+<!-- footer end -->
+
+</div>
+<!-- dynamic content end -->
+
+</div>
+<!-- app wrapper end -->
+
+<!-- jquery js -->
+<script src="js/plugins/jquery.min.js"></script>
+<!-- smooth scroll js -->
+<script src="js/plugins/smooth-scroll.js"></script>
+<!-- swup js -->
+<script src="js/plugins/swup.min.js"></script>
+<!-- swiper js -->
+<script src="js/plugins/swiper.min.js"></script>
+<!-- datepicker js -->
+<script src="js/plugins/datepicker.js"></script>
+<!-- isotope js -->
+<script src="js/plugins/isotope.js"></script>
+<!-- sticky -->
+<script src="js/plugins/sticky.js"></script>
+<!-- mapbox js -->
+<script src="js/plugins/mapbox.min.js"></script>
+<!-- fancybox js -->
+<script src="js/plugins/fancybox.min.js"></script>
+<!-- starbelly js -->
+<script src="js/main.js"></script>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,57 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/blog-details', function () {
+    return view('blog-details');
+});
+
+Route::get('/cart', function () {
+    return view('cart');
+});
+
+Route::get('/checkout', function () {
+    return view('checkout');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/faq', function () {
+    return view('faq');
+});
+
+Route::get('/gallery', function () {
+    return view('gallery');
+});
+
+Route::get('/menu', function () {
+    return view('menu');
+});
+
+Route::get('/product', function () {
+    return view('product');
+});
+
+Route::get('/reservation', function () {
+    return view('reservation');
+});
+
+Route::get('/reviews', function () {
+    return view('reviews');
+});
+
+Route::get('/shop', function () {
+    return view('shop');
 });
