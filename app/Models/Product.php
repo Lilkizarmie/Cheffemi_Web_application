105a59aab9eb54ef9176089bfe54aa058f8eda78
Lilkizarmie <jefferyadolor18@gmail.com>
2022-12-05 14:00:54 +0100

    initiated live wire admin panel and html template

---
+++ app/Models/Product.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Product extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'price',
+        'description',
+        'categories_id',
+        'tags',
+    ];
+
+    public function galleries()
+    {
+        return $this->hasMany(ProductGallery::class, 'products_id', 'id');
+    }
+
+    public function category()
+    {
+        return $this->hasOne(ProductCategory::class, 'categories_id', 'id');
+    }
+}
---
+++ app/Models/ProductCategory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+
+class ProductCategory extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+            'name',
+        ];
+    public function Products()
+    {
+        return $this->hasMany(Product::class, 'categories_id', 'id');
+    }
+
+    use HasFactory, SoftDeletes;
+}
+
